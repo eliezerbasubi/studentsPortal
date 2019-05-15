3255bcf66c6fc1ad701a93b72fb23c5f2545e5a6
Eliezer <eliezer.basubi30@gmail.com>
2019-05-15 10:22:06 +0300

    receiving files and upload them

--- pages/loadData.js
+++ pages/loadData.js
@@ -1,15 +1,21 @@
 const load =() => {
-    let xhr = new XMLHttpRequest()
-    xhr.open("GET","dump.php",true)
+    // let xhr = new XMLHttpRequest()
+    // xhr.open("GET","dump.php",true)
 
-    xhr.onreadystatechange = ()=>{
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // let json = JSON.parse(xhr.responseText)
-            console.log(xhr.responseText)
-        }
-    }
+    // xhr.onreadystatechange = ()=>{
+    //     if (xhr.readyState == 4 && xhr.status == 200) {
+    //         // let json = JSON.parse(xhr.responseText)
+    //         console.log(xhr.responseText)
+    //     }
+    // }
 
-    xhr.send()
+    // xhr.send()
+
+    fetch("dump.php").then((response)=>{
+        return response.text()
+    }).then(myJson =>{
+        console.log(JSON.stringify(myJson))
+    })
 }
 
 const onLoadEvent = () =>{
